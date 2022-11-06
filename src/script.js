a650f93edc211f86357b61d2e1ada39ff0635bc1
dgsilveira <dgsilveira@sga.pucminas.br>
2022-11-06 15:37:54 -0300

    Etapa 3 - Desenvolvimento da solução - Parte 1

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 100}%)`;
+
+}
+
+setInterval(carrossel, 5800);
+
+
+
+
